158d4f4e7529fb4905225c44e6605ba820766624
Santi1514 <dsantytrujillom@gmail.com>
2025-02-22 13:37:39 -0500

    Add files via upload

---
+++ Ejemplos c++.cpp
@@ -0,0 +1,75 @@
+//Ejemplo 1
+#include <iostream>
+using namespace std;
+// Entrada
+int main() {
+double numero;
+cin >> numero;
+//Proceso
+double cuadrado = numero * numero;
+//Salida
+cout << "El cuadrado del número es: " << cuadrado << endl;
+return 0;
+
+}
+
+//Ejemplo 2
+
+// Entrada
+int main() {
+    double numero;
+    cin >> numero;
+    //Proceso
+    double cubo = numero * numero * numero;
+    //Salida
+    cout << "El cubo del número es: " << cubo << endl;
+    return 0;
+    
+    }
+
+//Ejemplo 3
+
+// Entrada
+int main() {
+    double numero;
+    cin >> numero;
+    //Proceso
+    double operaciones = (((numero+5)-3)/4);
+    //Salida
+    cout << "El resultado de la operaciones es: " << operaciones << endl;
+    return 0;
+    
+    }    
+
+
+//Ejemplo 4
+
+// Entrada
+int main() {
+    double numero;
+    double b;
+    cin >> numero;
+    cin >> b;
+    //Proceso
+    double modulo = numero % b;
+    //Salida
+    cout << "El resultado del modulo es: " << modulo << endl;
+    return 0;
+    }
+
+// Ejemplo 5
+
+// Entrada
+int main() {
+    double numero;
+    double b;
+    cin >> numero;
+    cin >> b;
+    //Proceso
+    double multiplicacion = numero * b;
+    //Salida
+    cout << "El resultado de la multiplicacion es: " << multiplicacion << endl;
+    return 0;
+    }
+
+
---
+++ Ejemplos.py
@@ -0,0 +1,51 @@
+#Ejemplo 1
+#Calcular el cuadrado de un numero
+#Analisis del programa:
+
+#Entrada
+numero = float(input())
+#Proceso
+cuadrado = numero ** 2
+#Salida
+print("El cuadrado del número es:", cuadrado)
+
+#Ejemplo 2
+#Calcular el cubo de un numero
+#Entrada
+numero = float(input())
+#Proceso
+cubo = numero ** 3
+#Salida
+print("El cubo del número es:", cubo)
+
+#ejemplo 3 
+#Calcular un numero que se le sume 7, resten 6 y se divida en 5
+
+#Entrada
+numero = float(input())
+#Proceso
+operaciones = (((numero+7)-6)/5)
+#Salida
+print("El resultado de la operacion es:", operaciones)
+
+#ejemplo 4
+#Sacar el modulo
+
+#Entrada
+numero = float(input())
+b = float(input())
+#Proceso
+modulo = numero % b
+#Salida
+print("El modulo es:", modulo)
+
+#ejemplo 5
+#multiplicacion de 2 numeros
+
+#entrada
+n= float(input())
+b= float(input())
+#proceso
+r= n*b
+#salida
+print ("el resultado:",r)
